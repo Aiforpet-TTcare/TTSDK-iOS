ee49beeb2fcb603d4e14b6ed2dd1604098301087
Jay Lee <k.jaylee@gmail.com>
2025-09-03 13:44:13 +0900

    Update podspec version to 2.2.0

---
+++ TTSDK.xcframework/ios-arm64/AIScan.framework/Headers/AIScan.h
@@ -0,0 +1,18 @@
+//
+//  AIScan.h
+//  AIScan
+//
+//  Created by Jay Lee on 2/19/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AIScan.
+FOUNDATION_EXPORT double AIScanVersionNumber;
+
+//! Project version string for AIScan.
+FOUNDATION_EXPORT const unsigned char AIScanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AIScan/PublicHeader.h>
+
+
